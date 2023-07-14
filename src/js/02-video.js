1c652be15dd64dd41c8c333714de98bd3fe59668
Aleksandr <aik5787@gmail.com>
2023-07-14 14:19:13 -0400

    adding task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+const onPlay = throttle(function (data) {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+    }).catch(function(error) {
+    });
+}, 1000)
+    
+
+player.on('timeupdate', onPlay);
+
+
+const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+if (savedTime) {
+    player.setCurrentTime(savedTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+}
+
+
+
+
+
 
